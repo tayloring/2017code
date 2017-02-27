4f7d1257d90fe091503edd3870fabb29ef049d11
龚银超 <woduta@foxmail.com>
2017-02-27 16:01:51 +0800

    first commit

---
+++ app1.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+/*int main() {
+
+	
+	for (int  i = 0; i < 100000; i++) {
+
+		int  a=sqrt(i+100);
+		int b = sqrt(i + 268);
+		if (a*a == i + 100 && b*b == i + 268) {
+
+			printf("%d\n", i);
+		}
+		
+
+
+	}
+
+
+
+
+
+}*/
---
+++ app2.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+/*int main() {
+	
+
+	int a, b, c;
+	cout << "请输入年月日：\n";
+
+	cin >> a>>b>>c;
+
+	cout <<a<<b<<c<<"\n";
+
+	int h[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	
+
+	if (a % 4 == 0) {
+
+		int counts = 0;
+		
+		h[1] += 1;
+
+		b -= 1;
+		for (int i = 0; i < b; i++) {
+		
+
+			counts += h[i];
+			
+		
+		}
+
+		cout << "闰年";
+
+		cout << counts+c;
+		cout << "\n";
+	
+	
+	}
+	else {
+		int countn = 0;
+		b -= 1;
+		for (int i = 0; i < b; i++) {
+
+		
+
+			countn += h[i];
+
+			
+
+
+		}
+		int e=(countn + c);
+		cout << e;
+	
+	}
+
+	
+
+
+
+}*/
